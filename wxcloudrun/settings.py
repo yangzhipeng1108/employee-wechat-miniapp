6d6a3e4e4c638379f754471a90110649e9644d05
unknown <yangzhipeng1108@163.com>
2026-01-14 10:17:49 +0800

    config: 更新数据库默认配置为腾讯云MySQL实例
    - 修改默认密码为 admin@123
    - 修改默认主机为腾讯云MySQL实例地址
    - 修改默认端口为 29844
    - 配置为生产环境腾讯云数据库

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -74,9 +74,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get("MYSQL_DATABASE") or os.environ.get("MYSQL_DATABASE_NAME") or 'employee_management',
         'USER': os.environ.get("MYSQL_USERNAME") or os.environ.get("MYSQL_USER", 'root'),
-        'PASSWORD': os.environ.get("MYSQL_PASSWORD") or '',
-        'HOST': os.environ.get("MYSQL_HOST") or 'localhost',
-        'PORT': os.environ.get("MYSQL_PORT", '3306'),
+        'PASSWORD': os.environ.get("MYSQL_PASSWORD") or 'admin@123',
+        'HOST': os.environ.get("MYSQL_HOST") or 'sh-cynosdbmysql-grp-1k3d17lm.sql.tencentcdb.com',
+        'PORT': os.environ.get("MYSQL_PORT", '29844'),
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
